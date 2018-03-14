77be9facdda2f4b3e9c186a604aa4333a64e81ad
Annihilater <annihilater@icloud.com>
2018-03-14 21:07:04 +0800

    修复域名bug

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e@j(!y!rrs*d70^l$gj3k9ur%vs10!pxi9r$+4whqadhy3#avv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.schip.cc']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.schip.cc', '111.231.55.212']
 
 
 # Application definition
