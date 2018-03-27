ccad3774d8f99f6a556d3c236cca73fcdf424dcf
Annihilater <annihilater@icloud.com>
2018-03-27 15:03:53 +0800

    在侧边栏添加目录

--- blog/views.py
+++ blog/views.py
@@ -173,13 +173,23 @@ class PostDetailView(DetailView):
     def get_object(self, queryset=None):
         # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
         post = super(PostDetailView, self).get_object(queryset=None)
-        post.body = markdown.markdown(post.body,
-                                      extensions=[
-                                          'markdown.extensions.extra',
-                                          'markdown.extensions.codehilite',
-                                          'markdown.extensions.toc',
-                                      ])
+        # 在页面任何地方插入目录
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
         return post
+        #只能在文章开头插入目录
+        # post.body = markdown.markdown(post.body,
+        #                               extensions=[
+        #                                   'markdown.extensions.extra',
+        #                                   'markdown.extensions.codehilite',
+        #                                   'markdown.extensions.toc',
+        #                               ])
+        # return post
 
     def get_context_data(self, **kwargs):
         # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
