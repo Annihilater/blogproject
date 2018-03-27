91c813255cfe7800ff27bf3cab605366f755bb0d
Annihilater <annihilater@icloud.com>
2018-03-27 11:31:58 +0800

    修复admin分类、标签的显示bug

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,9 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 @python_2_unicode_compatible
 class Tag(models.Model):
@@ -28,6 +31,9 @@ class Tag(models.Model):
     再次强调一定要继承 models.Model 类！
     """
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 @python_2_unicode_compatible
@@ -44,8 +50,8 @@ class Post(models.Model):
     body = models.TextField()
 
     # 这两个列分别表示文章的创建时间和最后一次修改时间，存储时间的字段用 DateTimeField 类型。
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
 
     # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
     # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
@@ -98,4 +104,7 @@ class Post(models.Model):
 
         # 调用父类的 save 方法将数据保存到数据库中
         super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
