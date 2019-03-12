af525c580f2de25ab226361b4e3f08b6379b92c2
Annihilater <yanmiexingkong@gmail.com>
2019-03-13 04:44:17 +0800

    style: 为 django 设置 MySQL 数据库

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import secure
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -25,8 +25,12 @@ SECRET_KEY = 'e@j(!y!rrs*d70^l$gj3k9ur%vs10!pxi9r$+4whqadhy3#avv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.klause.cn']
-
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.klause.cn',
+    '.ivamp.cn',
+    '.pyhacker.cn']
 
 # Application definition
 
@@ -72,17 +76,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blogproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': secure.DATABASE,
+        'USER': secure.USER,
+        'PASSWORD': secure.PASSWORD,
+        'HOST': secure.HOST,
+        'PORT': secure.PORT,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -102,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -116,13 +124,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 HAYSTACK_CONNECTIONS = {
     'default': {
